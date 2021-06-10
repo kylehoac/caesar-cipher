d5e8a152dbcca204329e702a8bfe5f5bb20df250
Kyle Hoac <hoackyle@gmail.com>
2021-06-09 18:40:45 -0700

    fencrypt and decrypt working

---
+++ caesar_cipher/is_english_word.py
@@ -0,0 +1,36 @@
+import re
+from caesar_cipher.corpus_loader import word_list, name_list
+from caesar_cipher.caesar_cipher import possible
+candidates = possible
+# [
+#     "a dark and stormy night",
+#     "n qnex naq fgbezl avtug",
+#     "j mjat jwm bcxavh wrpqc",
+#     "call me Ishmael",
+#     "Billy Pilgrim has become unstuck in time",
+#     "All happy families are alike; each unhappy family is unhappy in its own way.",
+#     '"Where\'s Papa going with that ax?" said Fern to her mother as they were setting the table for breakfast.',
+#     "Off the hizzle fo shizzle",
+# ]
+
+
+def count_words(text):
+
+    candidate_words = text.split()
+
+    word_count = 0
+
+    for candidate in candidate_words:
+        word = re.sub(r"[^A-Za-z]+", "", candidate)
+        if word.lower() in word_list or word in name_list:
+            word_count += 1
+
+    return word_count
+
+
+for phrase in candidates:
+    recognized_word_count = count_words(phrase)
+    potential_word_count = len(phrase.split())
+    percentage = int(recognized_word_count / potential_word_count * 100)
+    if percentage >= 50:
+        print(phrase, percentage)
--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,5 +1,35 @@
-from caesar_cipher import __version__
+from caesar_cipher.caesar_cipher import encrypt,decrypt,crack
 
+def test_cipher_shift_1():
+    encrypted = encrypt("abcd",1)
+    assert encrypted == "bcde"
+    assert decrypt(encrypted, 1) == "abcd"
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_cipher_shift_2():
+    encrypted = encrypt("abcd",2)
+    assert encrypted == "cdef"
+    assert decrypt(encrypted, 2) == "abcd"
+
+def test_cipher_wrap():
+    encrypted = encrypt("xyz",1)
+    assert encrypted == "yza"
+    assert decrypt(encrypted, 1) == "xyz"
+    print("TEST PASSED")
+
+def test_cipher_crack():
+    encrypted = encrypt("bear", 2)
+    actual = crack(encrypted).__contains__("bear")
+    expected = True
+    assert actual == expected
+
+def test_cipher_crack_name():
+    encrypted = encrypt("billy", 2)
+    actual = crack(encrypted).__contains__("billy")
+    expected = True
+    assert actual == expected
+
+def test_cipher_with_capital():
+    encrypted = encrypt("KYLE", 2)
+    actual = crack(encrypted).__contains__("kyle")
+    expected = True
+    assert actual == expected
